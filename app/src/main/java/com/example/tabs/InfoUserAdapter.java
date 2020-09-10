2006d98c5352bd6aad6f01c7ee8c940070e17731
ZagNitin <nitin.gupta@zaggle.in>
2020-09-10 12:22:07 +0530

    Posts and Users Tabs Completed

--- app/src/main/java/com/example/tabs/InfoUserAdapter.java
+++ app/src/main/java/com/example/tabs/InfoUserAdapter.java
@@ -1,4 +1,66 @@
 package com.example.tabs;
 
-public class InfoUserAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class InfoUserAdapter extends RecyclerView.Adapter<InfoUserAdapter.InfoUserViewHolder> {
+
+
+    private List<User> userList;
+
+    public void setData(List<User> userList){
+        this.userList = userList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public InfoUserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.row_user_layout,parent,false);
+        return new InfoUserAdapter.InfoUserViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InfoUserViewHolder holder, int position) {
+        User user = userList.get(position);
+        int id = user.getId();
+        String name = user.getName();
+        String username = user.getUsername();
+        String email = user.getEmail();
+
+        holder.id.setText(""+id);
+        holder.name.setText(name);
+        holder.username.setText(username);
+        holder.email.setText(email);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+
+    public class InfoUserViewHolder extends RecyclerView.ViewHolder{
+        TextView id;
+        TextView name;
+        TextView username;
+        TextView email;
+
+        public InfoUserViewHolder(@NonNull View itemView) {
+            super(itemView);
+            id = (TextView) itemView.findViewById(R.id.textViewid);
+            name = (TextView) itemView.findViewById(R.id.textView9);
+            username = (TextView) itemView.findViewById(R.id.textView10);
+            email = (TextView) itemView.findViewById(R.id.textView11);
+        }
+    }
+
 }
--- app/src/main/java/com/example/tabs/UsersActivity.java
+++ app/src/main/java/com/example/tabs/UsersActivity.java
@@ -2,15 +2,29 @@ package com.example.tabs;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class UsersActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerViewUsers;
+    private InfoUserAdapter mInfoUserAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +34,14 @@ public class UsersActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
         bottomNavigationView.setSelectedItemId(R.id.users);
+
+        mRecyclerViewUsers = findViewById(R.id.recyclerViewUser);
+        mRecyclerViewUsers.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerViewUsers.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+
+        mInfoUserAdapter = new InfoUserAdapter();
+
+        getAllInfoUsers();
 
 
 
@@ -41,6 +63,25 @@ public class UsersActivity extends AppCompatActivity {
                         return true;
                 }
                 return false;
+            }
+        });
+    }
+    public void getAllInfoUsers() {
+        NetworkHandler.instance().mJsonPlaceHolderApi.users().enqueue(new Callback<List<User>>() {
+            @Override
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                if (response.isSuccessful()) {
+                    List<User> users = response.body();
+                    mInfoUserAdapter.setData(users);
+                    mRecyclerViewUsers.setAdapter(mInfoUserAdapter);
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<User>> call, Throwable t) {
+                Toast.makeText(UsersActivity.this, "Went wrong", Toast.LENGTH_SHORT).show();
+                Log.i("onFailure", "fail");
             }
         });
     }
